2da1feb52f89a601fa3450ad0c0dd4d6d61d9a56
acmenes <acmenescompositions@gmail.com>
2021-08-06 10:38:02 -0400

    finished model and working on main function

--- makeText.js
+++ makeText.js
@@ -1,2 +1,13 @@
 /** Command-line tool to generate Markov text. */
 
+const fs = require('fs')
+const axios = require('axios')
+const process = require('process')
+
+const markov = require('./markov')
+
+function createText(text){
+    let mm = new markov.MarkovMachine(text)
+    console.log(mm.createText())
+    return mm
+}
--- markov.js
+++ markov.js
@@ -7,6 +7,7 @@ class MarkovMachine {
 
   constructor(text) {
     let words = text.split(/[ \r\n]+/);
+    // let words = text.split(" ")
     this.words = words.filter(c => c !== "");
     this.makeChains();
   }
@@ -18,12 +19,38 @@ class MarkovMachine {
 
   makeChains() {
     // TODO
-  }
+    // use the map function, Map() holds key value pairs and will work here
 
+    let chains = new Map()
+    for(let i = 0; i < this.words.length; i++){
+      let word = this.words[i]
+      let nextWord = this.words[i + 1] || null;
+      if (chains.has(word)){
+        chains.get(word).push(nextWord)
+      }
+      else{
+        chains.set(word, [nextWord])
+      }
+    }
+      this.chains = chains
+  }
 
   /** return random text from chains */
 
   makeText(numWords = 100) {
-    // TODO
+    // first it should pick a random key from a set of keys
+    let keys = Array.from(this.chains.keys)
+    let key = Math.floor(Math.random() * keys.length) 
+    let output = []
+    while (output.length < numWords && key !== null ){
+      output.push(key)
+      key = this.chains.get(key)
+    }
+    return output.join(" ")
   }
-}
+
+  }
+
+
+module.exports = {
+  MarkovMachine, }
